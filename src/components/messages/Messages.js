9798d98eec51b960154b81d95db1dda65b7cb0df
Bihanskiy <bohdan.bihanskiy@gmail.com>
2022-10-27 17:54:33 +0300

    a4

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -61,7 +61,7 @@ function Messages() {
             a1
             fff
                 <div className="messages-layout__header layout-header">
-                b b b b b b b b b b A2 A3
+                b b b b b b b b b b A2 A3 A4
                 ggg
                     {/* head */}
                     <div className="layout-header__info">
